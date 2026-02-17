673bb22ec560337bfe797476f644b00322238238
Brian Dobbins <bdobbins@gmail.com>
2026-02-17 09:34:12 -0700

    Added optimized numpy version and fixed Makefile for correct flags

---
+++ scripts/single/run_numpy_optimized.sh
@@ -0,0 +1,15 @@
+#PBS -A CESM0029
+#PBS -N job_numpy_opt
+#PBS -q main
+#PBS -j oe
+#PBS -l walltime=00:05:00
+#PBS -l select=1:ncpus=128:mpiprocs=128:mem=235G
+
+SIZE=1024
+STEPS=10000
+
+module load conda
+conda activate /glade/work/bdobbins/conda/ci_compass
+
+mpirun python ${HOME}/ci_compass/src/numpy_optimized/heat_diffusion_mpi.py --size ${SIZE} --steps ${STEPS}
+
